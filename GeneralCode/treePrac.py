14b91c2fb745ffc3168b772e11a731d9dcffac0b
Derek <derek.ly28@gmail.com>
2018-12-30 16:27:30 -0700

    New

--- GeneralCode/treePrac.py
+++ GeneralCode/treePrac.py
@@ -40,8 +40,55 @@ def inorder(root):
         print(str(root.data))
         inorder(root.right)
 
-def printBFS(root):
+def height(root):
+    if root is None:
+        return -1
+    else:
 
+        l = height(root.left)
+        r = height(root.right)
+
+        return(max(l,r) + 1)
+
+
+def printLevel(root, i):
+    if root is None:
+        return
+
+    if i == 0:
+        print(str(root.data), end = " ")
+    else:
+        printLevel(root.left, i - 1)
+        printLevel(root.right, i - 1)
+
+
+def printBFSFunc(root):
+    h = height(root)
+
+    for i in range(h + 1):
+        printLevel(root, i)
+    print()
+
+arr = []
+def calculateAvg(root):
+    sum = 0
+
+    if root:
+        a = calculateAvg(root.left)
+        b = calculateAvg(root.right)
+        sum = a + b + root.data
+
+    return sum
+
+def traverse(root):
+    if root:
+        global arr
+        avg = calculateAvg(root)
+        arr.append(avg)
+        traverse(root.left)
+        traverse(root.right)
+
+def printBFSQueue(root):
     if root is None:
         return
 
@@ -49,8 +96,7 @@ def printBFS(root):
     queue.append(root)
 
     while len(queue) > 0:
-        print(queue[0].data)
-
+        print(queue[0].data, end = " ")
         n = queue.pop(0)
 
         if n.left is not None:
@@ -58,6 +104,8 @@ def printBFS(root):
 
         if n.right is not None:
             queue.append(n.right)
+
+    print()
 
 tree = BST()
 tree.insert(10)
@@ -67,4 +115,8 @@ tree.insert(4)
 tree.insert(1)
 tree.insert(6)
 #inorder(tree.root)
-printBFS(tree.root)
+#printBFSFunc(tree.root)
+#printBFSQueue(tree.root)
+
+traverse(tree.root)
+print(arr)
