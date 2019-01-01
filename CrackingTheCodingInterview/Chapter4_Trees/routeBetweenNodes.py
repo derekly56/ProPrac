da1266abddd30cfa0972fda46cdf810b871cfd26
Derek <derek.ly28@gmail.com>
2018-12-31 19:29:32 -0700

    Unfinished

---
+++ CrackingTheCodingInterview/Chapter4_Trees/routeBetweenNodes.py
@@ -0,0 +1,18 @@
+'''
+Author: Derek Ly
+Question: Route Between Nodes (Pg. 109)
+---------------------------------------------------------------------
+Given a directed graph, design ana lgorithm to find out whether there
+is a route between two nodes.
+---------------------------------------------------------------------
+'''
+
+class adjNode:
+    def __init__(self, value):
+        self.vertex = value
+        self.next = None
+
+class Graph:
+    def __init__(self, vertices):
+        self.V = vertices
+        self.graph = [None] * self.V
